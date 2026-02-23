3e9c8a6122db9c33e6f37d01a503aa26ea0feca5
GUSTAVO ADOLFO ACOSTA CARDONA <gustavoacosta1149850@correo.itm.edu.co>
2026-02-22 20:02:11 -0500

    Complete excercice 01

---
+++ LogicConcepts/IsNumberOddOrNot/Program.cs
@@ -0,0 +1,28 @@
+﻿var numberString = String.Empty;
+do  //do =>ciclo hacer o haga
+{
+    Console.Write("Ingrese numero o la palabra 'Salir' para salir: ");
+    numberString = Console.ReadLine(); // number = "45" ---  number = 45
+    if (numberString!.ToLower() == "salir")  // ToLower es convertir a minuscula
+    {
+        continue;
+    }
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El número {numberInt}, es par");  // $ interpolar combinar texto con numeros
+        }
+        else
+        {
+            Console.WriteLine($"El número {numberInt}, es impar");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Lo que ingresaste: {numberString}, no es un número entero.");
+    }
+} while (numberString!.ToLower() != "salir");
+Console.WriteLine("Game Over");
+
